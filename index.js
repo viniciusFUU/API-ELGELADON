6660f4d6eed8626be99933e0ab387b49f30ca9fe
vinicius3.98 <vinicius3_1998@hotmail.com>
2022-08-23 16:47:50 -0300

    dotenv configs

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express'); /* framework para facilitar para fazer as requisições*/
 const cors = require('cors'); /* para autorizar as rotas  */
 const routes = require('./src/routes/paleta.route');
 const connectToDatabase = require('./src/database/database');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 connectToDatabase();
@@ -13,5 +14,5 @@ app.use(cors());
 app.use('/paletas/', routes);
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
--- src/database/database.js
+++ src/database/database.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => {
   mongoose
-    .connect('mongodb+srv://root:admin@api-elgeladon.iuckgyt.mongodb.net/?retryWrites=true&w=majority', {
+    .connect(process.env.URI_DATABASE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => console.log('MongoDb conectado!'))
+    .then(() => console.log('MongoDb Atlas conectado!'))
     .catch((error) =>
       console.log(`Erro ao conectar com o MongoDb, erro: ${error}`),
     );
